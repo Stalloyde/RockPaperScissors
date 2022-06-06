e357880f8b7f8db9e098c3d4b3294686620cb5a0
Stalloyde <stalloyde93@gmail.com>
2022-06-06 11:06:43 +0800

    WIP - Add first to 5 game function

--- javascript.js
+++ javascript.js
@@ -2,88 +2,75 @@ let userScore = 0;
 let computerScore = 0;
 const userInput = "";
 
-
-
-
-
-//assigned action to parameters of computerInput and userInput in function playRound here so that entire program runs from scratch each loop
- function game () {
+function getComputerMove() {
+    let randomNumber = Math.floor(Math.random() * 3);
     
-    document.querySelector("#btn-rock").addEventListener("click", function () {displayInputs("Rock")});
-    document.querySelector("#btn-paper").addEventListener("click", function () {displayInputs("Paper")});
-    document.querySelector("#btn-scissors").addEventListener("click", function () {displayInputs("Scissors")});
-    
-    function displayInputs (userInput) {
-
-        function getComputerMove() {
-            let randomNumber = Math.floor(Math.random() * 3);
-            
-            if (randomNumber === 0) {
-                return 'Rock'; 
-                } else if
-            (randomNumber === 1) {
-                return 'Paper';
-                } else if
-            (randomNumber === 2) {
-                return 'Scissors';
-                }
-            }
-    
-            const computerMove = getComputerMove();
-
-            const userSelection = document.querySelector(".user-input");
-            const computerSelection = document.querySelector(".computer-input");
-            const roundResult = document.querySelector(".round-result");
-            const userScoreboard = document.querySelector(".scoreboard-user");
-            const computerScoreboard = document.querySelector(".scoreboard-computer");
-
-            userSelection.textContent = userInput;
-            computerSelection.textContent = computerMove;
-            roundResult.textContent = "";
-            userScoreboard.textContent = "";
-            computerScoreboard.textContent = "";
-
-            if (computerMove === userInput) {
-                roundResult.textContent = `IT'S A TIE!`
-                userScoreboard.textContent = `${userScore}`;
-                computerScoreboard.textContent = `${computerScore}`;
-            
-                } else if
-            
-            ((userInput === 'Rock' && computerMove === 'Scissors') || 
-            (userInput === 'Scissors' && computerMove === 'Paper') || 
-            (userInput === 'Paper' && computerMove === 'Rock')) {
-                
-                userScore++;
-        
-                roundResult.textContent =  `YOU WIN!`
-                userScoreboard.textContent = `${userScore}`;
-                computerScoreboard.textContent = `${computerScore}`;
-
-                } else if 
-            
-            ((computerMove === 'Rock' && userInput === 'Scissors') || 
-            (computerMove === 'Scissors' && userInput === 'Paper') || 
-            (computerMove === 'Paper' && userInput === 'Rock')) {
-            
-                computerScore++;
-        
-                roundResult.textContent = `COMPUTER WINS!`    
-                userScoreboard.textContent = `${userScore}`;
-                computerScoreboard.textContent = `${computerScore}`;}
-            
-            function gameOver () {
-                if (userScore === 5) {
-                    return scoreboardDiv.textContent = `YOU BEAT THE GAME! FINAL SCORE - User: ${userScore}, Computer: ${computerScore}`;
-                    } else if
-                (computerScore === 5) {
-                    return scoreboardDiv.textContent = `YOU GOT WHOOPED! FINAL SCORE - User: ${userScore}, Computer: ${computerScore}`;
-                }
-            }
+    if (randomNumber === 0) {
+        return 'Rock'; 
+        } else if
+    (randomNumber === 1) {
+        return 'Paper';
+        } else if
+    (randomNumber === 2) {
+        return 'Scissors';
+        }
     }
-}
 
-// end game. afer score === 5 .. trigger play again button or prompt ... something else other than alert function
+const btnRock = document.querySelector("#btn-rock");
+const btnPaper = document.querySelector("#btn-paper");
+const btnScissors = document.querySelector("#btn-scissors");
+btnRock.addEventListener("click", function () {displayInputs("Rock")});
+btnPaper.addEventListener("click", function () {displayInputs("Paper")});
+btnScissors.addEventListener("click", function () {displayInputs("Scissors")});
 
 
-game();
+function displayInputs (userInput) {
+
+        const computerMove = getComputerMove();
+        const userSelection = document.querySelector(".user-input");
+        const computerSelection = document.querySelector(".computer-input");
+        const roundResult = document.querySelector(".round-result");
+        const userScoreboard = document.querySelector(".scoreboard-user");
+        const computerScoreboard = document.querySelector(".scoreboard-computer");
+        userSelection.textContent = userInput;
+        computerSelection.textContent = computerMove;
+        roundResult.textContent = "";
+        userScoreboard.textContent = "";
+        computerScoreboard.textContent = "";
+        
+        if (computerMove === userInput) {
+            roundResult.textContent = `IT'S A TIE!`
+            userScoreboard.textContent = `${userScore}`;
+            computerScoreboard.textContent = `${computerScore}`;
+        
+            } else if
+        
+        ((userInput === 'Rock' && computerMove === 'Scissors') || 
+        (userInput === 'Scissors' && computerMove === 'Paper') || 
+        (userInput === 'Paper' && computerMove === 'Rock')) {
+            
+            userScore++;
+    
+            roundResult.textContent =  `YOU WIN!`
+            userScoreboard.textContent = `${userScore}`;
+            computerScoreboard.textContent = `${computerScore}`;
+            } else if 
+        
+        ((computerMove === 'Rock' && userInput === 'Scissors') || 
+        (computerMove === 'Scissors' && userInput === 'Paper') || 
+        (computerMove === 'Paper' && userInput === 'Rock')) {
+        
+            computerScore++;
+    
+            roundResult.textContent = `COMPUTER WINS!`    
+            userScoreboard.textContent = `${userScore}`;
+            computerScoreboard.textContent = `${computerScore}`;
+            }     
+        }
+
+if ((userScore === 5) || (computerScore === 5)) {
+    btnRock.removeEventListener("click", function () {displayInputs("Rock")});
+    btnPaper.removeEventListener("click", function () {displayInputs("Paper")});
+    btnScissors.removeEventListener("click", function () {displayInputs("Scissors")});
+    }
+    
